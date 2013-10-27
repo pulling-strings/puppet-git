90a5ed68d8d964922e2b8fa6b0581e64d66abac1
Ronen Narkis <narkisr@gmail.com>
2013-10-27 13:23:13 +0200

    adding timeout and fixing lint

--- manifests/clone.pp
+++ manifests/clone.pp
@@ -1,21 +1,23 @@
-define git::clone($url,$dst,$owner) {
+# clone git repos
+define git::clone($url,$dst,$owner,$timeout=360) {
 
   $to = basename($dst)
 
-  exec{"clone $name":
-    command => "git clone $url $to",
+  exec{"clone ${name}":
+    command => "git clone ${url} ${to}",
     cwd     => dirname($dst),
-    user    => "root",
+    user    => root,
     path    => ['/usr/bin/'],
-    unless  => "test -d $dst",
-    notify  => Exec["chown $name"],
-    require  => Package['git-core']
+    unless  => "test -d ${dst}",
+    notify  => Exec["chown ${name}"],
+    require => Package['git-core'],
+    timeout => $timeout
   }
 
-  exec{"chown $name":
-    command       => "chown $owner $dst -R",
-    user          => "root",
-    path    => ['/bin/'],
-    refreshonly   => true
+  exec{"chown ${name}":
+    command     => "chown ${owner} ${dst} -R",
+    user        => root,
+    path        => ['/bin/'],
+    refreshonly => true
   }
 }
