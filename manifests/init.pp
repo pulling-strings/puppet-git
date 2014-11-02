2ab657161c014bfc25ccd3ddd25de073f52341f8
narkisr <narkisr@gmail.com>
2014-11-02 03:51:11 +0200

    config should be seperate

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,7 @@
 # Git managment module
 class git($scm_user=false, $scm_email=false, $user=false) {
 
-  validate_string($scm_user)
-  validate_string($scm_email)
-  validate_string($user)
+  include git::params
 
-  if(!defined(Package['git-core'])){
-    package { 'git-core':
-      ensure => present
-    }
-  }
-
-  class { 'git::configure':
-    scm_user  => $scm_user,
-    scm_email => $scm_email
-  }
-
+  ensure_resource('package',$::git::params::package,{'ensure' => 'present'})
 }
