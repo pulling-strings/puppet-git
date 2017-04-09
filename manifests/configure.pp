88f68eae13b941f49f5976e35205e44aae32ad2e
ronen <narkisr@gmail.com>
2017-04-09 19:38:51 +1000

    removing git template

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -14,29 +14,22 @@ class git::configure(
 
   define git_config($value) {
     exec {"set git ${name} ${value}":
-      command     => "git config --global $name $value",
+      command     => "git config --global ${name} ${value}",
       path        => $::git::params::path,
       user        => 'root',
       environment => "HOME=${git::configure::home}",
       require     => Package[$::git::params::package],
-      unless      => "git config --get $name"
+      unless      => "git config --get ${name}"
     }
   }
 
   git_config{'user.name': value => $scm_user}
   git_config{ 'user.email': value => $scm_email}
   git_config{ 'core.editor': value => 'vim'}
-  git_config{ 'init.templatedir': value => '~/.git_template' }
   git_config {'alias.sub-add': value => "'submodule add'"}
   git_config {'alias.sub-up': value => "'submodule update --init'"}
   git_config {'alias.cm': value => "'commit -m'"}
   git_config {'alias.for-pull': value => "'submodule foreach git pull origin master'"}
   git_config {'alias.for-master': value => "'submodule foreach git checkout master'"}
-  git_config {'http.sslVerify': value => 'false'}
-
-  git::clone {'git_template':
-    url   => 'git://github.com/narkisr/git_template.git',
-    dst   => "${home}/.git_template",
-    owner => $user
-  }
+  git_config {'http.sslVerify': value => false}
 }
